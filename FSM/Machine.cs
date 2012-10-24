14e211997e3abf927c0d4c825249c90e89efc683
Achim Friedland <github@ahzf.de>
2012-10-24 15:08:08 +0200

    clean'ups

--- FSM/Machine.cs
+++ FSM/Machine.cs
@@ -1,59 +1,262 @@
-﻿using System;
-using System.Collections.Generic;
+﻿/*
+ * Copyright (c) 2010-2012, Achim 'ahzf' Friedland <achim@graph-database.org>
+ * This file is part of FSM <http://www.github.com/Vanaheimr/FSM>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#region Usings
+
+using System;
 using System.Linq;
-using System.Text;
+using System.Collections.Generic;
+
+#endregion
 
 namespace de.ahzf.Vanaheimr.FSM
 {
 
-    public class Machine<TStates, TSignal>
+    /// <summary>
+    /// A simple enum-based finite state machine.
+    /// </summary>
+    /// <typeparam name="TState">The type of the states.</typeparam>
+    /// <typeparam name="TSignal">The type of the signals.</typeparam>
+    public class FiniteStateMachine<TState, TSignal> where TState : struct
     {
 
-        private Dictionary<TStates, Dictionary<TSignal, Tuple<Action, TStates>>> Transitions;
+        #region Data
 
         private HashSet<TSignal> Signals;
+        
+        private Dictionary<TState, Dictionary<TSignal, Tuple<Action, TState>>> Transitions;
 
-        public TStates CurrentState { get; private set; }
+        #endregion
 
+        #region Properties
+
+        #region Name
+
+        /// <summary>
+        /// The name or identification for this finite state machine.
+        /// </summary>
         public String Name { get; private set; }
 
-        public TStates StartState { get; set; }
+        #endregion
 
-        public HashSet<TStates> AcceptingStates { get; private set; }
-        public HashSet<TStates> ErrorStates     { get; private set; }
-        public TStates ErrorState { get; private set; }
+        #region StartState
 
+        /// <summary>
+        /// The starting state.
+        /// </summary>
+        public TState StartState { get; private set; }
 
-        public Machine(String Name, TStates ErrorState)
+        private Boolean StartStateSet;
+
+        #endregion
+
+        #region CurrentState
+
+        /// <summary>
+        /// The current state.
+        /// </summary>
+        public TState CurrentState { get; private set; }
+
+        #endregion
+
+        #region AcceptingStates
+
+        private readonly HashSet<TState> _AcceptingStates;
+
+        /// <summary>
+        /// All accepting states of the finite state machine.
+        /// </summary>
+        public IEnumerable<TState> AcceptingStates
         {
-            this.Name = Name;
-            this.Transitions = new Dictionary<TStates, Dictionary<TSignal, Tuple<Action, TStates>>>();
-            this.Signals = new HashSet<TSignal>();
+            get
+            {
+                return _AcceptingStates;
+            }
+        }
 
-            this.AcceptingStates = new HashSet<TStates>();
-            this.ErrorStates     = new HashSet<TStates>();
-            this.ErrorState      = ErrorState;
-            ErrorStates.Add(ErrorState);
+        /// <summary>
+        /// Returns true if the finite state machine is in an accepting state.
+        /// </summary>
+        public Boolean InAcceptingState
+        {
+            get
+            {
+                return _AcceptingStates.Contains(CurrentState);
+            }
+        }
 
-            foreach (TStates state in Enum.GetValues(typeof(TStates)))
+        #endregion
+
+        #region ErrorStates
+
+        private readonly HashSet<TState> _ErrorStates;
+
+        /// <summary>
+        /// All error states including the golbal error state of the finite state machine.
+        /// </summary>
+        public IEnumerable<TState> ErrorStates
+        {
+            get
+            {
+                return _ErrorStates;
+            }
+        }
+
+        /// <summary>
+        /// The fatal error state.
+        /// </summary>
+        public TState FatalErrorState { get; private set; }
+
+        private Boolean FatalErrorStateSet;
+
+        /// <summary>
+        /// Returns true if the finite state machine is in an error state.
+        /// </summary>
+        public Boolean InErrorState
+        {
+            get
+            {
+                return ErrorStates.Contains(CurrentState);
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Constructor(s)
+
+        #region FiniteStateMachine()
+
+        /// <summary>
+        /// Create a new finite state machine.
+        /// </summary>
+        public FiniteStateMachine()
+            : this(Guid.NewGuid().ToString())
+        { }
+
+        #endregion
+
+        #region FiniteStateMachine(FSMName)
+
+        /// <summary>
+        /// Create a new finite state machine.
+        /// </summary>
+        /// <param name="FSMName">A name or identification for this finite state machine.</param>
+        public FiniteStateMachine(String FSMName)
+        {
+
+            if (!typeof(TState).IsEnum)
+                throw new Exception("TState is not an enum!");
+
+            if (!typeof(TSignal).IsEnum)
+                throw new Exception("TSignal is not an enum!");
+
+            this.Name                = FSMName;
+            this.Transitions         = new Dictionary<TState, Dictionary<TSignal, Tuple<Action, TState>>>();
+            this.Signals             = new HashSet<TSignal>();
+            this._AcceptingStates    = new HashSet<TState>();
+            this._ErrorStates        = new HashSet<TState>();
+            this.StartStateSet       = false;
+            this.FatalErrorStateSet  = false;
+
+            foreach (TState state in Enum.GetValues(typeof(TState)))
+            {
+                
                 AddState(state);
+                var memInfo = typeof(TState).GetMember(state.ToString());
+
+                var attr = memInfo[0].GetCustomAttributes(false);
+
+                if (attr.Where(_ => _ is StartStateAttribute).Any())
+                {
+
+                    if (StartStateSet)
+                        throw new Exception("Duplicate StartState!");
+
+                    StartState    = state;
+                    StartStateSet = true;
+
+                }
+
+                if (attr.Where(_ => _ is AcceptingStateAttribute).Any())
+                {
+
+                    if (_ErrorStates.Contains(state))
+                        throw new Exception("illegal!");
+
+                    _AcceptingStates.Add(state);
+
+                }
+
+                if (attr.Where(_ => _ is FatalErrorStateAttribute).Any())
+                {
+
+                    if (FatalErrorStateSet)
+                        throw new Exception("Duplicate FatalErrorState!");
+
+                    if (_AcceptingStates.Contains(state))
+                        throw new Exception("illegal!");
+
+                    FatalErrorState    = state;
+                    FatalErrorStateSet = true;
+
+                    _ErrorStates.Add(state);
+
+                }
+
+                if (attr.Where(_ => _ is ErrorStateAttribute).Any())
+                {
+
+                    if (_AcceptingStates.Contains(state))
+                        throw new Exception("illegal!");
+
+                    _ErrorStates.Add(state);
+
+                }
+
+            }
+
+
 
             foreach (TSignal signal in Enum.GetValues(typeof(TSignal)))
                 AddSignal(signal);
 
+
+            CurrentState = StartState;
+
         }
 
-        private TStates AddState(TStates State)
+        #endregion
+
+        #endregion
+
+
+        #region (private) AddState(State)
+
+        private TState AddState(TState State)
         {
-            Transitions.Add(State, new Dictionary<TSignal, Tuple<System.Action, TStates>>());
+            Transitions.Add(State, new Dictionary<TSignal, Tuple<System.Action, TState>>());
             return State;
         }
 
+        #endregion
 
-        public void SetState(TStates State)
-        {
-            CurrentState = State;
-        }
+        #region (private) AddSignal(State)
 
         private TSignal AddSignal(TSignal Signal)
         {
@@ -61,9 +264,19 @@ namespace de.ahzf.Vanaheimr.FSM
             return Signal;
         }
 
+        #endregion
 
 
-        public void AddTransition(TStates Source, TSignal Signal, Action Action, TStates Target)
+        #region AddTransition(Source, Signal, Action, Target)
+
+        /// <summary>
+        /// Adds a new transition.
+        /// </summary>
+        /// <param name="Source">The source state.</param>
+        /// <param name="Signal">The transition signal.</param>
+        /// <param name="Action">The transition action.</param>
+        /// <param name="Target">The target state.</param>
+        public void AddTransition(TState Source, TSignal Signal, Action Action, TState Target)
         {
 
             if (!Signals.Contains(Signal))
@@ -71,22 +284,27 @@ namespace de.ahzf.Vanaheimr.FSM
 
             var d1 = Transitions[Source];
 
-            d1.Add(Signal, new Tuple<Action, TStates>(Action, Target));
+            d1.Add(Signal, new Tuple<Action, TState>(Action, Target));
 
         }
 
+        #endregion
+
+        #region ProcessSignal(Signal)
+
+        /// <summary>
+        /// Process the incoming signal.
+        /// </summary>
+        /// <param name="Signal">A transition signal.</param>
         public void ProcessSignal(TSignal Signal)
         {
-
-            if (CurrentState == null)
-                CurrentState = StartState;
 
             if (ErrorStates.Contains(CurrentState))
                 return;
 
             var d1 = Transitions[CurrentState];
 
-            Tuple<Action, TStates> Tuple = null;
+            Tuple<Action, TState> Tuple = null;
 
             if (d1.TryGetValue(Signal, out Tuple))
             {
@@ -96,10 +314,12 @@ namespace de.ahzf.Vanaheimr.FSM
 
             else
             {
-                CurrentState = ErrorState;
+                CurrentState = FatalErrorState;
             }
 
         }
+
+        #endregion
 
     }
 
--- TestApplication1/Program.cs
+++ TestApplication1/Program.cs
@@ -8,14 +8,24 @@ namespace de.ahzf.Vanaheimr.FSM.TestApplication1
 
     public enum State
     {
+
+        [StartState]
         Start,
+
+        Middle,
+
+        [AcceptingState]
         End,
+
+        [FatalErrorState]
         Error
+
     }
 
     public enum Signal
     {
         Hello,
+        World,
         GoToHell
     }
 
@@ -25,10 +35,10 @@ namespace de.ahzf.Vanaheimr.FSM.TestApplication1
         public static void Main(String[] Args)
         {
 
-            var FSM1 = new Machine<State, Signal>("FSM1", State.Error) { StartState = State.Start };
-//            FSM1.ErrorStates.Add(States.Error);
+            var FSM1 = new FiniteStateMachine<State, Signal>("FSM1");
 
-            FSM1.AddTransition(State.Start, Signal.Hello, () => Console.WriteLine("Hello received!"), State.End);
+            FSM1.AddTransition(State.Start,  Signal.Hello, () => Console.WriteLine("Hello received!"), State.Middle);
+            FSM1.AddTransition(State.Middle, Signal.World, () => Console.WriteLine("World received!"), State.End);
 
             Console.WriteLine(FSM1.CurrentState);
 
